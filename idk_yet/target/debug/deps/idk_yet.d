3232d5cf5c2c9e6e16ecc4beb52bef192122f8f4
MartinMandzak <mandzak.martin3@gmail.com>
2023-10-30 11:08:32 +0100

    added my first rust project template

---
+++ idk_yet/target/debug/deps/idk_yet.d
@@ -0,0 +1,5 @@
+C:\Users\mandz\Desktop\GitHub Projects\rust_projects\idk_yet\target\debug\deps\idk_yet.exe: src\main.rs
+
+C:\Users\mandz\Desktop\GitHub Projects\rust_projects\idk_yet\target\debug\deps\idk_yet.d: src\main.rs
+
+src\main.rs:
---
+++ idk_yet/target/debug/idk_yet.d
@@ -0,0 +1,1 @@
+C:\Users\mandz\Desktop\GitHub\ Projects\rust_projects\idk_yet\target\debug\idk_yet.exe: C:\Users\mandz\Desktop\GitHub\ Projects\rust_projects\idk_yet\src\main.rs
