cb9aa4e8a2282ad27fe6045beaee54b452479c83
MartinMandzak <mandzak.martin3@gmail.com>
2024-04-10 12:23:58 +0200

    im bored now

--- todoList/src/main.rs
+++ todoList/src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+
+struct Book{
+    author: String,
+    title: String,
+    is_checked_out: bool
+}
+
+struct Library{
+    shelves: Vec<Book>
+}
+
+impl Library{
+    fn new() -> Library{Library{shelves: Vec::new(),}}
+
+    fn add(&mut self, author: String, title: String){
+        self.shelves.push(Book {author, title, is_checked_out: false});
+    }
+
+    fn add(&mut self, book: Book){
+        self.shelves.push(book);
+    }
+
 }
